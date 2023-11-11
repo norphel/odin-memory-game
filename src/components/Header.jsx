f1091b3fa6d1bbfa3ac8820cc89170809b8fbbd7
Norphel <113838216+norphel@users.noreply.github.com>
2023-11-11 22:22:14 +0530

    add static header and main elements

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Memory Card Game</h1>
+            <div className="scores">
+                <p>Highest Score: </p>
+                <p>Current Score: </p>
+            </div>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+    return (
+        <main>
+            <div className="cardsContainer"></div>
+        </main>
+    )
+}
