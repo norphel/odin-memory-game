ee114a3fa36d005b7bab3251d1962cf075dd4e7e
Norphel <113838216+norphel@users.noreply.github.com>
2023-11-15 15:34:54 +0530

    add handleClick event to start button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,31 @@
-// import Header from './Header.jsx';
-// import Main from './Main.jsx';
+import { useState } from 'react';
+
+import Header from './Header.jsx';
+import Main from './Main.jsx';
 import '../styles/App.css'
 
 
 export default function App() {
-    return (
-        <>
-            <h1 id='title'>Hogwarts Memory Magic</h1>
-            <button id='startButton'>Start</button>
-            {/* <Header />
-            <Main /> */}
-        </>
-    )
+    const [gameStart, setGameStart] = useState(false);
+
+    function handleClick() {
+        setGameStart(true);
+    }
+    
+    if (gameStart === false) {
+        return (
+            <>
+                <h1 id='title'>Hogwarts Memory Magic</h1>
+                <button id='startButton' onClick={ handleClick }>Start</button>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <Header />
+                <Main />
+            </>
+        )
+    }
+    
 }
