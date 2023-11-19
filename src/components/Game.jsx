db632cad2367e8b77be4c4ac183b63d15e7db6f9
Norphel <113838216+norphel@users.noreply.github.com>
2023-11-19 18:40:09 +0530

    add scores

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,21 @@
 export default function Game() {
     return (
         <>
-            <h1>Hogwarts Memory Magic</h1>
+            <div className="titleAndScore">
+                <h1>Hogwarts Memory Magic</h1>
+                <table className="score">
+                    <tr>
+                        <th>Current Score</th>
+                        <td>0</td>
+                    </tr>
+                    <tr>
+                        <th>Best Score</th>
+                        <td>0</td>
+                    </tr>
+                </table>
+            </div>
             <div className="container">
-                
+
             </div>
         </>
     )
