7c7acc8c57e28b1b56974acb824cba426c9e5e98
Norphel <113838216+norphel@users.noreply.github.com>
2023-11-21 22:11:24 +0530

    fetch images in useEffect

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,3 +1,47 @@
+import { useState, useEffect } from "react"
+
+function MemoryGameCards() {
+    const [images, setImages] = useState([]);
+    
+    useEffect(() => {
+        const fetchImages = async () => {
+            try {
+                const response = await fetch("https://hp-api.onrender.com/api/characters/");
+                if (!response.ok) {
+                    throw new Error("Failed to fetch images");
+                }
+                const allImages = await response.json();
+                const filteredImages = allImages.filter(image => image.image);
+                const requiredImages = getRandomSubset(filteredImages, 10);
+                setImages(requiredImages);
+            } catch (error) {
+                console.error("Error fetching images: ", error);
+            }
+        };
+        fetchImages();
+    }, []);
+    
+    const getRandomSubset = (arr, size) => {
+        const shuffled = arr.sort(() => 0.5 - Math.random());
+        return shuffled.slice(0, size);
+    }
+
+    return (
+        <div className="container">
+            {images.map((image, index) => (
+                <div className="card" key={index}>
+                    <img 
+                        src={image.image} 
+                        alt={image.name}
+                        className="card-image" 
+                    />
+                    <div className="image-name">{image.name}</div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
 export default function Game() {
     return (
         <>
@@ -16,18 +60,7 @@ export default function Game() {
                     </tbody>
                 </table>
             </div>
-            <div className="container">
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
-            </div>
+            <MemoryGameCards />
         </>
     )
 }
